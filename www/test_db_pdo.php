4b24322cb79a84a937f6f84624ff8d8762bb3c60
Kevin Pareek <kevinpareek@gmail.com>
2024-11-18 15:04:06 +0530

    feat(proxy): add Nginx reverse proxy and improve configuration

--- www/test_db_pdo.php
+++ www/test_db_pdo.php
@@ -1,12 +1,15 @@
 <?php
 
-$DBuser = 'root';
-$DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
+
 $pdo = null;
+$MYSQL_HOST="database";
+$MYSQL_DATABASE="docker";
+$MYSQL_USER="docker";
+$MYSQL_PASSWORD="docker";
 
 try{
-    $database = 'mysql:host=database:3306';
-    $pdo = new PDO($database, $DBuser, $DBpass);
+    $database = 'mysql:host='.$MYSQL_HOST.':3306';
+    $pdo = new PDO($database, $MYSQL_USER, $MYSQL_PASSWORD);
     echo "Success: A proper connection to MySQL was made! The docker database is great.";    
 } catch(PDOException $e) {
     echo "Error: Unable to connect to MySQL. Error:\n $e";
